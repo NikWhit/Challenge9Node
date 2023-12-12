402fa696bad002a4aab9994c261c8de5358d349a
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-11 22:11:35 -0700

    uploading

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Project Title?\n',
+        name: 'Title',
+    },
+    {
+        type: 'input',
+        message: 'Repo Title?\n',
+        name: 'repoTitle',
+    },
+    {
+        type: 'input',
+        message: 'What is your project about?\n',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the deployed URL?\n',
+        name: 'deployedWebsite',
+    },
+    {
+        type: 'input',
+        message: 'Instructions for install?\n',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Upload a screenshot?\n',
+        name: 'screenshot',
+    },
+    {
+        type: 'list',
+        message: 'Please select a license.\n',
+        name: 'license',
+        choices: ['None', 'MIT', 'GNU', 'ISC', 'Apache']
+    },
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let dir = './output';
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+        fs.writeFile(`./output/${fileName}`, data, (err) =>
+    err ? console.log(err) : console.log('README.md generated!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log(`This is README Generator, Answer questions to proceed.`);
+    inquirer
+    .createPromptModule(questions)
+    .then((answers) => {
+        const markdown = generateMarkdown(answers);
+            writeToFile(`README.md`, markdown);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,94 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(answers) {
+  return `${answers.title && `# ${answers.title}`}}
+  ${
+    answers.repoTitle&&
+    `[![License] (https://img.shield.io/static/v1?lavel=License&message+${answers.license}$color=blue&style=plastic&logo+appveyor)](https://opensource.org/license/${answers.license})\n\n`
+  }
+  ${answers.title && `## Table Of Content\n`}
+  ${answers.description && `- [Description](#description)`}
+  ${answers.deployedWebsite && `- [Deployed website link](#deployedWebsite)`}
+  ${answers.installation && `- [Installation](#installation)`}
+  ${answers.usage && `- [Usage](#usage)`}
+  ${answers.contribution && `- [Contributing](#contribution)`}
+  ${answers.tests && `- [Tests](#tests)`}
+  ${answers.github && `- [GitHub](#github)`}
+  ${answers.email && `- [Contact](#contact)`}
+  ${answers.repoTitle && `- [License](#license)`}
+  \n\n
+
+  ${
+    answers.librariesAndTools &&
+    `${answers.title} libraries: <ul>${answers.librariesAndTools}</ul>
+  \n\n`
+  }
+  \n
+
+  ${
+    answers.description &&
+    `## Description\n
+    ${answers.description}
+    \n\n`
+  }\n
+  
+  ${
+    answers.deployedWebsite &&
+      `<p></p><a href="${answers.deployedWebsite}">${answers.deployedWebsite}</a></p>\n\n`
+  }
+  \n
+  
+
+  ${
+    answers.repoTitle &&
+    `![GitHub repo size](https://img.shields.io/github/repo-size/${answers.github}/${answers.repoTitle}?style=plastic)\n`
+  }
+  ${
+  answers.repoTitle&&
+  `## License\n
+  [![License](https://img.shields.io/static/v1?lavel=License&message=${answers.license}&color=blue)]{https://opensoiurce.org/license/${answers.license})\n\n`}
+    \n
+    ${
+      answers.installation &&
+      `## Installation\n
+    ${answers.installation}
+    \n\n`
+    }
+    ${
+      answers.tests &&
+      `## Tests\n 
+    ${answers.tests}
+    \n\n`
+    }
+    \n
+    ${
+      answers.contribution &&
+      `## Contribution\n 
+    ${answers.contribution}
+    \n`}`;
+    }
+    // function generateMarkdown(answers) {
+    module.exports = generateMarkdown;
+
+// function renderLicenseBadge(license) {
+
+//   }
+// }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+// function renderLicenseLink(license) {
+
+// #Description
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
 
-module.exports = generateMarkdown;
+  // 
